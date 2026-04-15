b8296de9df00a68b5243eb38ce81de826896d461
greening777 <greeninggongbu@gmail.com>
2026-04-15 14:17:37 +0900

    bfs

--- 4 graph.py
+++ 4 graph.py
@@ -4,22 +4,37 @@
 class graphM:
     def __init__(self, size):
         self.mat=[[0 for _ in range(size)] for _ in range(size)]
+        self.size=size
     def add_edge_un(self,src,dst):
         self.mat[src][dst]=1
         self.mat[dst][src]=1
     
-    def dfs_graph(self, node, visited=None):
+    def dfs_mat(self, node, visited=None):
         if visited is None:
             visited=[]
         visited.append(node)
         print(node, end='->')
         for i in range(0, len(self.mat)):
             if self.mat[node][i]==1 and i not in visited:
-                self.dfs_graph(i,visited)
+                self.dfs_mat(i,visited)
             
+    def bfs_mat(self,node):
+        visited=[0]*self.size
+        visited[node]=1
+        q=[node]
+        while q:
+            curr=q.pop(0)
+            print(curr, end="->")
+            for i in range(0, len(self.mat)):
+                if self.mat[curr][i]==1 and visited[i]==0:
+                    q.append(i)
+                    visited[i]=1
+                    
     
     def add_edge_dir(self, src, dst):
         self.mat[src][dst] =1
+        
+    
     def print_graph(self):
         print(end="  ")
         for i in range(0, len(self.mat)):
@@ -32,24 +47,39 @@ class graphM:
             print(" ")
             
 
-graph = graphM(5)
-graph.add_edge_un(0,1)
-graph.add_edge_un(1,3)
-graph.add_edge_un(2,0)
-graph.add_edge_un(2,1)
-graph.add_edge_un(2,3)
-graph.add_edge_un(3,2)
-graph.add_edge_un(3,4)
-graph.print_graph()
+# graph = graphM(5)
+# graph.add_edge_un(0,1)
+# graph.add_edge_un(1,3)
+# graph.add_edge_un(2,0)
+# graph.add_edge_un(2,1)
+# graph.add_edge_un(2,3)
+# graph.add_edge_un(3,2)
+# graph.add_edge_un(3,4)
+# graph.print_graph()
 
-graph.dfs_graph(0)
+# graph.dfs_mat(0)
+# print("")
+
+graph=graphM(9)
+graph.add_edge_un(0,1)
+graph.add_edge_un(0,3)
+graph.add_edge_un(1,2)
+graph.add_edge_un(2,5)
+graph.add_edge_un(3,4)
+graph.add_edge_un(3,6)
+graph.add_edge_un(4,7)
+graph.add_edge_un(5,8)
+graph.add_edge_un(6,7)
+graph.dfs_mat(0)
 print("")
+graph.bfs_mat(0)
 
 ######리스트 사용 그래프
 
 class graphL:
     def __init__(self,size ):
         self.ad=[[] for _ in range(size)]
+        self.size=size
         
     def add_edge_dir(self, src, dst):
         self.ad[src].append(dst)
@@ -58,25 +88,65 @@ class graphL:
         self.ad[src].append(dst)
         self.ad[dst].append(src)
         
-    def dfs_list(self, node, visited):
+    def dfs_list(self, node, visited): #recursive
         visited[node]=1
         print(node, end="->")
         for i in self.ad[node]:
            if visited[i]==0:
                self.dfs_list(i, visited) 
+    def dfs_list_iter(self, node):
+        visited=[0]*self.size
+        st=[node]
+        while st:
+            curr=st.pop()
+            if visited[curr]==0:
+                visited[curr]=1
+                print(curr, end="->")
+                for i in self.ad[curr]:
+                    if visited[i]==0:
+                        st.append(i)
+        
+    
+    def bfs_list(self, node):
+        visited=[0]*self.size
+        visited[node]=1
+        q=[node]
+        while q:
+            curr=q.pop(0)
+            print(curr, end="->")
+            for i in self.ad[curr]:
+                if visited[i]==0:
+                    visited[i]=1
+                    q.append(i)
+                    
+        
+    
+    
+# al=graphL(5)
+# al.add_edge_un(0,1)
+# al.add_edge_un(1,3)
+# al.add_edge_un(2,0)
+# al.add_edge_un(2,1)
+# al.add_edge_un(2,3)
+# al.add_edge_un(3,4)
+# print(al.ad)
 
-al=graphL(5)
+# visited=[0]*5
+# al.dfs_list(0, visited)
+# print("")
+
+
+
+al=graphL(9)
 al.add_edge_un(0,1)
-al.add_edge_un(1,3)
-al.add_edge_un(2,0)
-al.add_edge_un(2,1)
-al.add_edge_un(2,3)
+al.add_edge_un(0,3)
+al.add_edge_un(1,2)
+al.add_edge_un(2,5)
 al.add_edge_un(3,4)
-print(al.ad)
-
-visited=[0]*5
-al.dfs_list(0, visited)
+al.add_edge_un(3,6)
+al.add_edge_un(4,7)
+al.add_edge_un(5,8)
+al.add_edge_un(6,7)
 print("")
-
-
-
+print(al.ad)
+visited=[0]*9
--- 4q.py
+++ 4q.py
@@ -1,22 +1,40 @@
+####인접행렬 그래프
+
+
 class graphM:
     def __init__(self, size):
         self.mat=[[0 for _ in range(size)] for _ in range(size)]
+        self.size=size
     def add_edge_un(self,src,dst):
         self.mat[src][dst]=1
         self.mat[dst][src]=1
     
-    def dfs_graph(self, node, visited=None):
+    def dfs_mat(self, node, visited=None):
         if visited is None:
             visited=[]
         visited.append(node)
         print(node, end='->')
         for i in range(0, len(self.mat)):
             if self.mat[node][i]==1 and i not in visited:
-                self.dfs_graph(i,visited)
+                self.dfs_mat(i,visited)
             
+    def bfs_mat(self,node):
+        visited=[0]*self.size
+        visited[node]=1
+        q=[node]
+        while q:
+            curr=q.pop(0)
+            print(curr, end="->")
+            for i in range(0, len(self.mat)):
+                if self.mat[curr][i]==1 and visited[i]==0:
+                    q.append(i)
+                    visited[i]=1
+                    
     
     def add_edge_dir(self, src, dst):
         self.mat[src][dst] =1
+        
+    
     def print_graph(self):
         print(end="  ")
         for i in range(0, len(self.mat)):
@@ -28,10 +46,14 @@ class graphM:
                 print(self.mat[i][j], end=" ")
             print(" ")
             
-            
+
+
+######리스트 사용 그래프
+
 class graphL:
     def __init__(self,size ):
         self.ad=[[] for _ in range(size)]
+        self.size=size
         
     def add_edge_dir(self, src, dst):
         self.ad[src].append(dst)
@@ -40,32 +62,129 @@ class graphL:
         self.ad[src].append(dst)
         self.ad[dst].append(src)
         
-    def dfs_list(self, node, visited):
+    def dfs_list(self, node, visited): #recursive
         visited[node]=1
         print(node, end="->")
         for i in self.ad[node]:
            if visited[i]==0:
                self.dfs_list(i, visited) 
+    def dfs_list_iter(self, node):
+        visited=[0]*self.size
+        st=[node]
+        while st:
+            curr=st.pop()
+            if visited[curr]==0:
+                visited[curr]=1
+                print(curr, end="->")
+                for i in self.ad[curr]:
+                    if visited[i]==0:
+                        st.append(i)
+        
+    
+    def bfs_list(self, node):
+        visited=[0]*self.size
+        visited[node]=1
+        q=[node]
+        while q:
+            curr=q.pop(0)
+            print(curr, end="->")
+            for i in self.ad[curr]:
+                if visited[i]==0:
+                    visited[i]=1
+                    q.append(i)
+                    
+        
+    
 
 
-##quiz4
 
-n= int(input())
-m= int(input())
+# ##quiz4 - dfs
 
-L=[[] for _ in range(n+1)]
+# n= int(input())
+# m= int(input())
 
-for i in range(0, m):
-    src, dst= map(int, input().split())
-    L[src].append(dst)
-    L[dst].append(src)
+# L=[[] for _ in range(n+1)]
 
-visited=[0]*(n+1)
-def dfs_worm(L,node,visited):
-    visited[node]=1
-    for i in L[node]:
-        if visited[i]==0:
-            dfs_worm(L,i,visited)
+# for i in range(0, m):
+#     src, dst= map(int, input().split())
+#     L[src].append(dst)
+#     L[dst].append(src)
 
-dfs_worm(L,1,visited)
-print(sum(visited)-1)
+# visited=[0]*(n+1)
+# def dfs_worm(L,node,visited):
+#     visited[node]=1
+#     for i in L[node]:
+#         if visited[i]==0:
+#             dfs_worm(L,i,visited)
+
+# dfs_worm(L,1,visited)
+# print(sum(visited)-1)
+
+
+# ###############bfs
+# n= int(input())
+# m= int(input())
+
+# L=[[] for _ in range(n+1)]
+
+# for i in range(0, m):
+#     src, dst= map(int, input().split())
+#     L[src].append(dst)
+#     L[dst].append(src)
+    
+# def bfs_p1(L,start):
+#     v=[0]*(n+1)
+#     v[start]=1
+#     q=[start]
+#     while q:
+#         curr=q.pop(0)
+#         for i in L[curr]:
+#             if v[i]==0:
+#                 v[i]=1
+#                 q.append(i)
+#     return sum(v)-1
+
+# print(bfs_p1(L,1))
+
+
+
+#####quiz4-2
+
+#bfs
+def bfs_s(L,start,target):
+    visited=[0]*len(L)
+    visited[start]=1
+    q=[start]
+    dis=[0]*len(L)
+    while q:
+        curr=q.pop(0)
+        if curr==target:
+            return dis[curr]
+        
+        for i in L[curr]:
+            if visited[i]==0:
+                visited[i]=1
+                q.append(i)
+                dis[i]=dis[curr]+1
+    return -1 
+
+L=[[1, 3], [0, 2], [1, 5], [0, 4, 6], [3, 7], [2, 8], [3, 7], [4, 6], [5]]
+print(bfs_s(L,0,4))
+
+#dfs
+def dfs_d(L,start,target):
+    visited=[0]*len(L)
+    dis=[0]*len(L)
+    st=[start]
+    while st:
+        curr=st.pop()
+        if visited[curr]==0:
+            visited[curr]=1
+            
+            for i in L[curr]:
+                if visited[i]==0:
+                    dis[i]=dis[curr]+1
+                    st.append(i)
+    return dis[target]
+
+print(dfs_d(L,0,2))
